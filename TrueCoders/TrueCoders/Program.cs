5572fafd3ded5806596f1ac3b0fa2244da4995b9
RodGaddie <54855621+RodGaddie@users.noreply.github.com>
2019-09-17 13:51:13 -0500

    Finished

---
+++ TrueCoders/TrueCoders/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TrueCoders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var charlie = new people();
+            var harrison = new people();
+            var troy = new people();
+            var rod = new people();
+
+            charlie.Age = 25;
+            charlie.FirstName = "Charlie";
+            charlie.LastName = "Doyle";
+            charlie.HairColor = "Black";
+
+            harrison.Age = 18;
+            harrison.FirstName = "Harrison";
+            harrison.LastName = "Rodriguez";
+            harrison.HairColor = "Brown";
+
+            troy.Age = 30;
+            troy.FirstName = "Troy";
+            troy.LastName = "Maxwell";
+            troy.HairColor = "brown";
+
+            rod.Age = 30;
+            rod.FirstName = "Rod";
+            rod.LastName = "Gaddie";
+            rod.HairColor = "Brown";
+
+            List<people> People = new List<people>();
+
+            People.Add(charlie);
+            People.Add(harrison);
+            People.Add(troy);
+            People.Add(rod);
+
+            foreach (people person in People)
+            {
+                Console.WriteLine($"Name: {person.FirstName} {person.LastName}, Age: {person.Age}, Haircolor: {person.HairColor}");
+            }
+        }
+    }
+}
---
+++ TrueCoders/TrueCoders/people.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrueCoders
+{
+    class people
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string HairColor { get; set; }
+        public int Age { get; set; }
+    }
+}
